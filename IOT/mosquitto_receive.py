55c0d1b0b9779710549352f86a5e69f4ebbffe35
gerossil <leo.gerossier38@gmail.com>
2023-01-05 16:31:33 +0100

    test

--- IOT/mosquitto_receive.py
+++ IOT/mosquitto_receive.py
@@ -1,14 +1,30 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt
+import random #Pour générer des valeurs aléatoires
+import time #Pour attendre entre chaque envoi
+ 
 
-broker_address="172.20.10.2" 
 
-print("creating new instance")
+MQTT_SERVER = "localhost"
+MQTT_PORT = "1883"
+MQTT_TOPIC = "capteur"
 
-client = mqtt.Client("P1") #create new instance
-print("connecting to broker")
+client = mqtt.Client() #Création client MQTT
 
-client.connect(broker_address, 1883) #connect to broker
+try :
+    client.connect(MQTT_SERVER,MQTT_PORT)
+except :
+    print("Connection to MQTT server failed")
+    exit()
 
-print("Subscribing to topic","Emergency")
-
-client.subscribe("Emergency")
+try:
+    for i in range(0, 6):
+        for j in range(0, 10):
+            x = random.randint(0, 10)
+            point="capteur,tag=value x="+ j +",longitude="+i+",intensity="+x
+            client.publish(MQTT_TOPIC,point)
+            time.sleep(0.1)
+except:
+    print("Publishing Error")
+    exit()
+client.loop_start()
+client.disconnect()
