03a913faf2c38ce0cd00c8046cf9bba1d726eb35
gerossil <leo.gerossier38@gmail.com>
2023-01-06 15:43:52 +0100

    IOT

--- IOT/Passerelle_Simulation.py
+++ IOT/Passerelle_Simulation.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python
 import psycopg2
+import serial
+import time
 
 
+
+SERIALPORT = "COM4"
+BAUDRATE = 115200
+ser = serial.Serial()
+
+
+def initUART():     
+          
+        # ser = serial.Serial(SERIALPORT, BAUDRATE)
+        ser.port=SERIALPORT
+        ser.baudrate=BAUDRATE
+        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+        ser.parity = serial.PARITY_NONE #set parity check: no parity
+        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+        ser.timeout = None          #block read
+
+        # ser.timeout = 0             #non-block read
+        # ser.timeout = 2              #timeout block read
+        ser.xonxoff = False     #disable software flow control
+        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+        #ser.writeTimeout = 0     #timeout for write
+        print ("Starting Up Serial Monitor")
+        try:
+                ser.open()
+        except serial.SerialException:
+                print("Serial {} port not available".format(SERIALPORT))
+                exit()
+
+
+def sendUARTMessage(msg):
+    ser.write(msg.encode())
 
 
 def connect():
@@ -27,7 +61,9 @@ def connect():
         cur.execute('SELECT * FROM Lieux WHERE intensite > 0')
 
         for row in cur.fetchall():
-            print(row.id)
+            x = (int(row[1]), int(row[2]), int(row[3]))
+            sendUARTMessage(x) 
+            
 
         # display the PostgreSQL database server version
 
