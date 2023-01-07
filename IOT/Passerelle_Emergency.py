a0ea8096301ad7e563aa3b7f962cf3b259dd0c56
gerossil <leo.gerossier38@gmail.com>
2023-01-07 15:37:46 +0100

    push modif

--- IOT/Passerelle_Emergency.py
+++ IOT/Passerelle_Emergency.py
@@ -38,6 +38,47 @@ def on_publish(client,userdata,result):
     print("Donnée Publiee \n")
     pass
 
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+            host="127.0.0.1",
+            database="emergency",
+            user="pgtp",
+            password="tp"
+        )
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT * FROM Lieux WHERE intensite > 0')
+
+        
+        db_version = cur.fetchall()
+        print(db_version)
+
+            
+
+        # display the PostgreSQL database server version
+
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
 if __name__ == '__main__':
         initUART()
         print ('Press Ctrl-C to quit.')
@@ -56,6 +97,9 @@ if __name__ == '__main__':
                                 client.on_publish = on_publish
                                 client.connect(broker_address)
                                 client.publish("feu",x)
+
+                                #PostgreSQL
+                                connect(x)
 
                                 LAST_VALUE = data_str
                                 print(data_str)
