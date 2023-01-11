72c179491d4e2ab879b76ca12cc524c8f58e5f92
gerossil <leo.gerossier38@gmail.com>
2023-01-12 00:01:16 +0100

    push

--- WEB/Emergency/controller/controller_emergency.php
+++ WEB/Emergency/controller/controller_emergency.php
@@ -12,8 +12,12 @@ class Emergency extends ControllerTwig{
         $emergency = new Emergency_BDD();
         $result = $emergency->Emergency_Localisation_Feux_BDD();
         $camion = $emergency->Emergency_Localisation_Camion_BDD();
+        $nb = $emergency->Emergency_Nb_Interventions();
+        $total_feux = $emergency->Emergency_Nb_Feux();
+        $camion_dispo = $emergency->Emergency_Camion_Dispo();
+        $nb_feux_interventions = $emergency->Emergency_Nb_Feux_Interventions();
         if($result != null || $camion != null){
-            $this->renderWithParams('acceuil', ['result' => $result, 'camion' => $camion]);
+            $this->renderWithParams('acceuil', ['result' => $result, 'camion' => $camion, 'nb' => $nb, 'total_feux' => $total_feux, 'camion_dispo' => $camion_dispo, 'nb_feux_interventions' => $nb_feux_interventions]);
         }
         else{
             $this->render('acceuil');
@@ -26,6 +30,7 @@ class Emergency extends ControllerTwig{
     public function map(){
         Emergency_Localisation_Feux();
         Emergency_Localisation_Camion();
+        Emergency_Nb_Interventions();
     }
 }
 
--- WEB/Emergency/model/pathologie/Emergency_BDD.php
+++ WEB/Emergency/model/pathologie/Emergency_BDD.php
@@ -27,4 +27,36 @@ class Emergency_BDD  extends Model{
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+
+    public function Emergency_Nb_Interventions(){
+        $query = "SELECT COUNT(*) AS nb_interventions FROM interventions where etat = 0";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function Emergency_Nb_Feux(){
+        $query = "SELECT COUNT(*) AS nb_feux FROM lieux WHERE intensite > 0";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function Emergency_Camion_Dispo(){
+        $query = "SELECT COUNT(*) AS nb_camion FROM camions WHERE etat = 0";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function Emergency_Nb_Feux_Interventions(){
+        $query = "SELECT COUNT(*) AS nb_feux_interventions FROM Interventions INNER JOIN LIEUX ON interventions.lieu = lieux.id and interventions.etat = 1";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
